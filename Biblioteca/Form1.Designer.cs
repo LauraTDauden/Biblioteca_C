4e98330a3b7d1307c2c5508d0c03e9ea0dd83662
Laura Torres <xians_heritage@hotmail.com>
2021-10-23 13:25:20 +0100

    Creando vista alumnos

--- Biblioteca/Form1.Designer.cs
+++ Biblioteca/Form1.Designer.cs
@@ -46,9 +46,9 @@ namespace Biblioteca
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(10, 252);
+            this.dataGridView1.Location = new System.Drawing.Point(10, 228);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(778, 187);
+            this.dataGridView1.Size = new System.Drawing.Size(778, 211);
             this.dataGridView1.TabIndex = 0;
             // 
             // label1
